4a0f0b8035c338ad10a4a6f827bbdffcdefd0fc0
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-03-11 11:23:40 +0100

    feat(back:entities): UserToken

---
+++ backend/src/entities/UserToken.ts
@@ -0,0 +1,30 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@ObjectType()
+@Entity({ name: "user_token" })
+export class UserToken {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @PrimaryColumn({ type: "varchar", length: 255 })
+  token: string;
+
+  @Field({ nullable: true })
+  @Column({ type: "varchar", length: 255, nullable: true })
+  refresh_token?: string;
+
+  @ManyToOne(() => User, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+}
