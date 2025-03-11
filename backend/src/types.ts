014f418efdba34d430275c6258940c7aa88bd8a5
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-03-11 16:59:39 +0100

    feat(back:jest): add tests directory + basic tests

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,38 +1,38 @@
-import {IncomingMessage, ServerResponse} from "http";
+import { IncomingMessage, ServerResponse } from "http";
 
 export type AdType = {
-    id: number;
-    title: string;
-    description?: string;
-    owner: string;
-    price: number;
-    picture: string;
-    location: string;
-    createdAt: string;
+  id: number;
+  title: string;
+  description?: string;
+  owner: string;
+  price: number;
+  picture: string;
+  location: string;
+  createdAt: string;
 };
 
 export type Category = {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 };
 
 export type TagType = {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 };
 
 export type UserType = {
-    id: number;
-    email: string;
-    password: string;
-    firstname: string;
-    lastname: string;
-    role: RoleType;
+  id: number;
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  role: RoleType;
 };
 
 export type ContextType = {
-    req: IncomingMessage;
-    res: ServerResponse<IncomingMessage>;
+  req: IncomingMessage;
+  res: ServerResponse<IncomingMessage>;
 };
 
 export type AuthContextType = ContextType & { user: UserType };
@@ -40,6 +40,6 @@ export type AuthContextType = ContextType & { user: UserType };
 // export type Role = "admin" | "moderator" | "user";
 
 export enum RoleType {
-    USER = "user",
-    ADMIN = "admin"
+  USER = "user",
+  ADMIN = "admin",
 }
