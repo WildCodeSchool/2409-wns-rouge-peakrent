935c4fd65a18ba3b6afe8f291787057640e49f33
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-03-11 16:58:32 +0100

    refactor(back): buildSchema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import "reflect-metadata";
-import {dataSource} from "./config/db";
-import {ApolloServer} from "@apollo/server";
-import {startStandaloneServer} from "@apollo/server/standalone";
-import {buildSchema} from "type-graphql";
-import {CategoryResolver} from "./resolver/Categories";
-import {ProductResolver} from "./resolver/Products";
-import {SearchResolver} from "./resolver/Searchs";
-import {UserResolver} from "./resolver/Users";
-import {authChecker} from "./auth";
-import {VariantResolver} from "./resolver/Variants";
-import {ProfileResolver} from "./resolver/Profiles";
+import { dataSource } from "./config/db";
+import { getSchema } from "./schema";
 
 const port = 4000;
 
 const initialize = async () => {
-    const schema = await buildSchema({
-        resolvers: [
-            UserResolver,
-            ProfileResolver,
-            CategoryResolver,
-            ProductResolver,
-            SearchResolver,
-            VariantResolver
-        ],
-        authChecker,
-    });
-    const server = new ApolloServer({schema});
-    await dataSource.initialize();
-    const {url} = await startStandaloneServer(server, {
-        listen: {port: port},
-        context: async ({req, res}) => {
-            return {req, res};
-        },
-    });
-    console.log(`Server ready at: ${url} 🚀`);
+  const schema = await getSchema();
+  const server = new ApolloServer({ schema });
+  await dataSource.initialize();
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: port },
+    context: async ({ req, res }) => {
+      return { req, res };
+    },
+  });
+  console.log(`Server ready at: ${url} 🚀`);
 };
+
 initialize();
