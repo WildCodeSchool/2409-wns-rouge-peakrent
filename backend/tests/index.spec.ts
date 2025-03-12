6898e733e3c9756c6d0d77ea23e4e6b3dfa5c0b8
L0Bot <botterilouis@gmail.com>
2025-03-12 17:12:29 +0100

    Creating test users before running all test + new script for windows

--- backend/tests/index.spec.ts
+++ backend/tests/index.spec.ts
@@ -5,6 +5,7 @@ import { getSchema } from "../src/schema";
 
 // import { CategoriesResolverTest } from "./resolvers/CategoriesResolver";
 import { UsersResolverTest } from "./resolvers/UsersResolver";
+import { User } from "../src/entities/User";
 
 export type TestArgsType = {
   server: ApolloServer<BaseContext>;
@@ -21,6 +22,70 @@ const testArgs: TestArgsType = {
 export function assert(expr: unknown, msg?: string): asserts expr {
   if (!expr) throw new Error(msg);
 }
+
+export const setupTestUsers = async (testArgs: TestArgsType) => {
+  console.log(
+    "------------------------------------------------------ Setting up test users ------------------------------------------------------"
+  );
+  const userResponse = await testArgs.server.executeOperation<{
+    createUser: User;
+  }>({
+    query: `
+      mutation CreateUser($data: UserCreateInput!) {
+        createUser(data: $data) {
+          id
+          email
+          firstname
+          lastname
+          role
+        }
+      }
+    `,
+    variables: {
+      data: {
+        email: "user@example.com",
+        password: "SuperSecret!2025",
+        confirmPassword: "SuperSecret!2025",
+        firstname: "Regular",
+        lastname: "User",
+      },
+    },
+  });
+
+  const adminResponse = await testArgs.server.executeOperation<{
+    createUser: User;
+  }>({
+    query: `
+      mutation CreateUser($data: UserCreateInput!) {
+        createUser(data: $data) {
+          id
+          email
+          firstname
+          lastname
+          role
+        }
+      }
+    `,
+    variables: {
+      data: {
+        email: "admin@example.com",
+        password: "SuperSecret!2025",
+        confirmPassword: "SuperSecret!2025",
+        firstname: "Admin",
+        lastname: "User",
+      },
+    },
+  });
+
+  assert(userResponse.body.kind === "single");
+  testArgs.data.user = userResponse.body.singleResult.data?.createUser;
+
+  assert(adminResponse.body.kind === "single");
+  testArgs.data.admin = adminResponse.body.singleResult.data?.createUser;
+  console.log(
+    "------------------------------------------------------ Test users created ------------------------------------------------------"
+  );
+};
 
 beforeAll(async () => {
   await dataSource.initialize();
@@ -39,9 +104,11 @@ beforeAll(async () => {
   const testServer = new ApolloServer({
     schema,
   });
+  testArgs.server = testServer;
+
+  await setupTestUsers(testArgs);
 
   testArgs.dataSource = dataSource;
-  testArgs.server = testServer;
 });
 
 describe("users resolver", () => {
