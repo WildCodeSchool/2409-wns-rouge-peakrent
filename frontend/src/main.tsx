f1e9932e3dc2215e33552d952ee37d5d7ad79580
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-07 16:48:22 +0200

    refactor(frontend): replace SCSS with global CSS and update TailwindIndicator responsiveness

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/index.scss";
+import "./styles/global.css";
 import { TailwindIndicator } from "./utils/TailwindIndicator.tsx";
 
 const client = new ApolloClient({
