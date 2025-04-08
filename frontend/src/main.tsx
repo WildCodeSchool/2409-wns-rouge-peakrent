94eead1073b6ae3c65c7ee96834fc15769e2efd8
SAURET Mathieu <143399672+SAURETMathieu@users.noreply.github.com>
2025-04-08 12:21:47 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/index.scss";
+import "./styles/global.css";
+import { TailwindIndicator } from "./utils/TailwindIndicator.tsx";
+
+const client = new ApolloClient({
+  // uri: "http://localhost:5050/",
+  uri: "/api",
+  cache: new InMemoryCache(),
+  credentials: "same-origin",
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+      <TailwindIndicator />
+    </ApolloProvider>
   </StrictMode>
 );
