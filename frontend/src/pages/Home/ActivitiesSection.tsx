c6df5e10746ab2870f04f0946538369616498480
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-08 15:28:09 +0200

    feat(home): refactor ActivitiesSection for improved layout and componentization
    - Introduced ActivityItem component to encapsulate activity rendering logic.
    - Enhanced layout structure for better responsiveness and spacing.
    - Split activities into two sections for improved visual organization.

--- frontend/src/pages/Home/ActivitiesSection.tsx
+++ frontend/src/pages/Home/ActivitiesSection.tsx
@@ -1,6 +1,41 @@
 import { ImageHandler } from "@/components/ui/tables/columns/components/ImageHandler";
+import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 import { homeActivities } from "./fakeData";
+
+const ActivityItem = ({
+  activity,
+  index,
+}: {
+  activity: any;
+  index?: number;
+}) => {
+  return (
+    <a
+      key={activity.title}
+      href={`/activities/${activity.title}`}
+      className={cn(
+        "aspect-square relative hover:cursor-pointer group overflow-hidden col-span-1",
+        index !== undefined &&
+          index !== null &&
+          `col-start-[${index + 2}] col-span-2`
+      )}
+    >
+      <div className="w-full h-full overflow-hidden rounded-full border">
+        <ImageHandler
+          src={activity.image}
+          alt={activity.title}
+          className="w-full h-full object-cover hover:scale-110 transition-all duration-300"
+        />
+        <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 hidden md:flex items-center justify-center rounded-full">
+          <p className="text-white text-lg font-semibold text-center px-4">
+            {activity.title}
+          </p>
+        </div>
+      </div>
+    </a>
+  );
+};
 
 export default function ActivitiesSection() {
   const [windowWidth, setWindowWidth] = useState(0);
@@ -23,28 +58,26 @@ export default function ActivitiesSection() {
         Activités
       </h2>
 
-      <div className="relative space-y-8">
-        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4">
-          {homeActivities.slice(0, getActivitiesCount()).map((activity) => (
-            <a
-              key={activity.title}
-              href={`/activities/${activity.title}`}
-              className="aspect-square relative hover:cursor-pointer group overflow-hidden"
-            >
-              <div className="w-full h-full overflow-hidden rounded-full border">
-                <ImageHandler
-                  src={activity.image}
-                  alt={activity.title}
-                  className="w-full h-full object-cover hover:scale-110 transition-all duration-300"
+      <div className="flex flex-col gap-0">
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-2 sm:gap-4">
+          {homeActivities
+            .slice(0, Math.ceil(getActivitiesCount() / 2))
+            .map((activity) => (
+              <ActivityItem key={activity.title} activity={activity} />
+            ))}
+        </div>
+        <div className="grid grid-cols-6 sm:grid-cols-8 md:grid-cols-10 gap-4 mx-auto">
+          {homeActivities
+            .slice(Math.ceil(getActivitiesCount() / 2), getActivitiesCount())
+            .map((activity, index) => {
+              return (
+                <ActivityItem
+                  key={activity.title}
+                  activity={activity}
+                  index={index}
                 />
-                <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 hidden md:flex items-center justify-center rounded-full">
-                  <p className="text-white text-lg font-semibold text-center px-4">
-                    {activity.title}
-                  </p>
-                </div>
-              </div>
-            </a>
-          ))}
+              );
+            })}
         </div>
       </div>
     </section>
