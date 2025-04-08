3e7180af42eb313d8d2351f841b0f7fe5898e1cf
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-08 13:59:57 +0200

    feat(home): update HomePage layout with responsive components

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,18 +1,26 @@
 import { Carousel } from "@/components/ui/carousel/Carousel";
+import useBreakpoints from "@/hooks/useBreakpoint";
+import ActivitiesBentoGrid from "./ActivitiesBentoGrid";
 import ActivitiesSection from "./ActivitiesSection";
-import ForwardProducts from "./ForwardProducts";
 import { carousselImages } from "./fakeData";
+import ForwardProducts from "./ForwardProducts";
+import RentalProcessFlow from "./RentalProcess";
 
 export function HomePage() {
+  const { lg } = useBreakpoints();
   return (
     <>
       <Carousel
         images={carousselImages}
-        className="border-b-2 border-secondary"
+        className="border-b-2"
         imageClassName="object-cover"
       />
-      <ActivitiesSection />
-      <ForwardProducts />
+      <div className="container mx-auto px-4 max-w-screen-xl flex flex-col gap-4">
+        {lg ? <ActivitiesBentoGrid /> : <ActivitiesSection />}
+
+        <ForwardProducts />
+        <RentalProcessFlow />
+      </div>
     </>
   );
 }
