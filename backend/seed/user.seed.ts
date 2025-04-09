6157079bec572f455a9fc83aca7ce212d056699f
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-09 13:51:32 +0200

    fix(seed): update admin password to include an extra exclamation mark

--- backend/seed/user.seed.ts
+++ backend/seed/user.seed.ts
@@ -10,7 +10,7 @@ export const seedUsers = async () => {
   const users = [
     {
       email: "admin@peakrent.com",
-      password: "Admin123!",
+      password: "Admin123!!",
       firstname: "Admin",
       lastname: "User",
       role: RoleType.ADMIN,
