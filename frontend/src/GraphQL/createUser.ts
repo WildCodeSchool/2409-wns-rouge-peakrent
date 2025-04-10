a371cafdc2c61a656109b54a755bbae938ea5f75
CharlotteR678 <charlotte.rouyer@hotmail.fr>
2025-04-10 15:00:20 +0200

    test create_user

--- frontend/src/GraphQL/createUser.ts
+++ frontend/src/GraphQL/createUser.ts
@@ -1,9 +1,13 @@
 import { gql } from "@apollo/client/core";
 
-export const CREATE_USER = gql`
+export const CREATE_USER = `
   mutation Mutation($data: UserCreateInput!) {
     createUser(data: $data) {
       id
     }
   }
+`;
+
+export const CREATE_USER_FRONT = gql`
+  ${CREATE_USER}
 `;
