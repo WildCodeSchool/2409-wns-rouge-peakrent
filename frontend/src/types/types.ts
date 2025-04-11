2fb61d5e539a538268eed42a6b408f8ea15d097b
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-11 10:46:02 +0200

    feat(types): add VariantType and update ProductType to include variants

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,19 +2,30 @@ export type CategoryType = {
   id: number;
   name: string;
   description?: string;
+  children?: CategoryType[];
   image?: string;
   createdAt: Date;
   updatedAt: Date;
+};
+
+export type VariantType = {
+  id: number;
+  size?: string;
+  color?: string;
+  pricePerHour: number;
+  discount?: number;
 };
 
 export type ProductType = {
   id: number;
   name: string;
   description: string;
+  discount: number;
   urlImage: string;
   isPublished: boolean;
   sku: string;
   categories: CategoryType[];
+  variants: VariantType[];
   createdAt: Date;
   updatedAt: Date;
 };
