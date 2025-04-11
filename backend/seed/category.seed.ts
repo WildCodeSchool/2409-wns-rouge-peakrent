b1f88120a83b612a0408c559eeb11c52ba5700eb
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-11 14:56:20 +0200

    feat(categories): update category images and enhance getCategories query to return paginated results

--- backend/seed/category.seed.ts
+++ backend/seed/category.seed.ts
@@ -16,23 +16,38 @@ export const seedCategories = async () => {
   const categories = [
     {
       name: "Ski Alpin",
-      urlImage: "https://example.com/images/ski-alpin.jpg",
+      urlImage:
+        "https://images.unsplash.com/photo-1649404173620-a2ca20fcd562?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNraSUyMGFscGlufGVufDB8fDB8fHww",
     },
     {
       name: "Snowboard",
-      urlImage: "https://example.com/images/snowboard.jpg",
+      urlImage:
+        "https://plus.unsplash.com/premium_photo-1708834155836-4eec278332b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25vd2JvYXJkfGVufDB8fDB8fHww",
     },
     {
       name: "Raquettes",
-      urlImage: "https://example.com/images/raquettes.jpg",
+      urlImage:
+        "https://images.unsplash.com/photo-1728081931259-093429cf8467?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhcXVldHRlc3xlbnwwfHwwfHx8MA%3D%3D",
     },
     {
       name: "Chaussures",
-      urlImage: "https://example.com/images/chaussures.jpg",
+      urlImage:
+        "https://unsplash.com/fr/photos/une-paire-de-chaussures-de-ski-posee-dans-la-neige-NXTSa2L7xTs",
     },
     {
       name: "Vêtements",
-      urlImage: "https://example.com/images/vetements.jpg",
+      urlImage:
+        "https://unsplash.com/fr/photos/personne-avec-des-skis-prete-pour-les-pistes-EqIbeYzpQus",
+    },
+    {
+      name: "Vélos",
+      urlImage:
+        "https://images.unsplash.com/photo-1633707167682-9068729bc84c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8VlRUJTIwbW9udGFnbmV8ZW58MHx8MHx8fDA%3D",
+    },
+    {
+      name: "Randonnée",
+      urlImage:
+        "https://plus.unsplash.com/premium_photo-1661814278311-d59ab0b4a676?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9ubiVDMyVBOWVzfGVufDB8fDB8fHww",
     },
   ];
 
