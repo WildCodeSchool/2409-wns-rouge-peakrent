06994b278b3610e69173f302c73e67cdff6f4682
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-15 10:25:20 +0200

    feat(adminCategoriesPage): add button to open CategoryForm modal for creating new categories

--- frontend/src/pages/Admin/Categories/AdminCategoriesPage.tsx
+++ frontend/src/pages/Admin/Categories/AdminCategoriesPage.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@/components/ui/button";
 import CategoriesTable from "./CategoriesTable";
+import { Plus } from "lucide-react";
+import { useModal } from "@/context/modalProvider";
+import { CategoryForm } from "./CategoryForm";
 
 export function AdminCategoriesPage() {
+  const { openModal } = useModal();
   return (
     <div>
-      <h1 className="text-4xl font-bold">Catégories</h1>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold leading-none tracking-tight">
+        Catégories
+        </h1>
+        <Button onClick={() => openModal(<CategoryForm />)}>
+          <Plus size={16} className="mr-2" />
+          Nouvelle catégorie
+        </Button>
+      </div>
       <section className="mt-5">
         <CategoriesTable />
       </section>
