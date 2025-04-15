f7b2d46393810afa77eb03017d484454a47747c9
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-15 10:24:33 +0200

    refactor(regex): format cityRegex for improved readability

--- frontend/src/schemas/regex.ts
+++ frontend/src/schemas/regex.ts
@@ -11,6 +11,7 @@ export const zipCodeRegex = /^[a-z0-9][a-z0-9\- ]{0,10}[a-z0-9]$/i;
 
 export const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s-]+$/;
 
-export const cityRegex = /^[A-Za-zÀ-ÖØ-öø-ÿĀ-žȘșȚțŁłčęėįųūăîâşţșțäöüßéèêëçîïôûùÿñ\s'-]+$/;
+export const cityRegex =
+  /^[A-Za-zÀ-ÖØ-öø-ÿĀ-žȘșȚțŁłčęėįųūăîâşţșțäöüßéèêëçîïôûùÿñ\s'-]+$/;
 
 export const addressRegex = /^[A-Za-z0-9À-ÖØ-öø-ÿ\s,.-]+$/;
