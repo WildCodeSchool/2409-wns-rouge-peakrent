24195c62d0849ff4f7650ddd527c3e0e52dbe718
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-04-18 10:23:42 +0200

    feat(CategoriesActions): integrate CategoryForm for editing categories in admin panel

--- frontend/src/pages/Admin/Categories/CategoriesActions.tsx
+++ frontend/src/pages/Admin/Categories/CategoriesActions.tsx
@@ -2,6 +2,7 @@ import { Row } from "@tanstack/react-table";
 
 import DeleteButton from "@/components/buttons/DeleteButton";
 import UpdateButton from "@/components/buttons/UpdateButton";
+import { CategoryForm } from "./CategoryForm";
 
 interface DataTableRowCategoriesActionsProps<TData> {
   row: Row<TData>;
@@ -11,17 +12,14 @@ export function DataTableRowCategoriesActions<TData>({
   row,
 }: DataTableRowCategoriesActionsProps<TData>) {
   const category = row.original as any;
-
   const handleDelete = async (ids: string[] | number[]) => {
     return true;
   };
 
-  const formContent = <div>Form content</div>;
-
   return (
     <div className="col-span-2 flex items-center justify-center gap-2 p-2">
       <UpdateButton
-        modalContent={formContent}
+        modalContent={<CategoryForm datas={category} />}
         ariaLabel={"editCategoryAriaLabel"}
         variant="primary"
         modalTitle="Modifier la catégorie"
