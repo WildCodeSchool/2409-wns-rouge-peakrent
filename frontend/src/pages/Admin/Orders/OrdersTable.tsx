8f5bbc71717470cb2c59590a25fde206c83f0ea0
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-08-25 16:53:28 +0200

    refactor(Orders): update order actions and columns for improved functionality

--- frontend/src/pages/Admin/Orders/OrdersTable.tsx
+++ frontend/src/pages/Admin/Orders/OrdersTable.tsx
@@ -5,20 +5,25 @@ import Table from "@/components/ui/tables/Table";
 import { GET_ORDERS_ADMIN } from "@/graphQL/order";
 import { useOrderStore } from "@/stores/admin/order.store";
 import { ColumnConfig } from "@/types/datasTable";
+import { getOrdersWithDates } from "@/utils/getOrdersWithDates";
 import { gql, useQuery } from "@apollo/client";
 import { toast } from "sonner";
 import { createColumns } from "./ordersColumns";
 
 export default function OrdersTable() {
+  // Store states
   const ordersStore = useOrderStore((state) => state.orders);
   const ordersFetched = useOrderStore((state) => state.ordersFetched);
   const setOrders = useOrderStore((state) => state.setOrders);
   const setOrdersFetched = useOrderStore((state) => state.setOrdersFetched);
 
+  // GraphQL query
   const { data, error, loading } = useQuery(gql(GET_ORDERS_ADMIN));
 
+  // Column configs
   const columnConfigs: ColumnConfig[] = [];
 
+  // Effect to fetch orders
   useEffect(() => {
     if (error) {
       toast.error("Erreur lors de la récupération des produits");
@@ -32,6 +37,7 @@ export default function OrdersTable() {
     }
   }, [data, error, setOrders, setOrdersFetched]);
 
+  // Skeleton loading
   if (loading || !ordersFetched) {
     return (
       <DataTableSkeleton
@@ -46,25 +52,28 @@ export default function OrdersTable() {
     );
   }
 
-  const onDeleteMultipleFunction = async (ids: string[] | number[]) => {
-    return true;
-  };
+  // const onDeleteMultipleFunction = async (ids: string[] | number[]) => {
+  //   return true;
+  // };
+
+  // Format orders with dates
+  const ordersWithDates = getOrdersWithDates(ordersStore);
 
   return (
     <Table
-      data={ordersStore}
+      data={ordersWithDates}
       columns={createColumns}
       columnConfigs={columnConfigs}
       filterTextOptions={{
-        id: "id",
-        placeholder: `"id"`,
+        id: "reference",
+        placeholder: `Référence / "id"`,
       }}
       hideColumns={{
         sku: false,
       }}
       rowLink="id"
       hideExport
-      onDeleteMultipleFunction={onDeleteMultipleFunction}
+      // onDeleteMultipleFunction={onDeleteMultipleFunction}
     />
   );
 }
--- frontend/src/pages/Admin/Orders/ordersColumns.tsx
+++ frontend/src/pages/Admin/Orders/ordersColumns.tsx
@@ -1,17 +1,23 @@
 import * as column from "@/components/ui/tables/columns";
 import { ColumnDef, FilterFn } from "@tanstack/react-table";
 
+import { getOrderStatusText, getOrderStatusVariant } from "@/utils";
+import { getTotalOrderPrice } from "@/utils/getTotalOrderPrice";
 import { DataTableRowOrdersActions } from "./OrderActions";
 
 const multiColumnFilter: FilterFn<any> = (row, columnId, filterValue) => {
   const id = String(row.getValue("id"));
+  const reference = String(row.getValue("reference"));
   const searchText = filterValue.toLowerCase();
 
-  return '"' + id?.toLowerCase() + '"' === searchText;
+  return (
+    '"' + id?.toLowerCase() + '"' === searchText ||
+    reference?.toLowerCase().includes(searchText)
+  );
 };
 
 export const createColumns: ColumnDef<any>[] = [
-  column.createSelectColumn(),
+  // column.createSelectColumn(),
 
   column.createStringColumn({
     id: "id",
@@ -20,6 +26,41 @@ export const createColumns: ColumnDef<any>[] = [
     className: "text-start",
     enableSorting: true,
     enableHiding: true,
+  }),
+
+  column.createBadgeColumn({
+    id: "statut",
+    accessorKey: "status",
+    title: "Statut",
+    enableSorting: true,
+    labelFn: (row) => getOrderStatusText(row.original.status),
+    variantFn: (row) => getOrderStatusVariant(row.original.status),
+  }),
+
+  column.createStringColumn({
+    id: "reference",
+    accessorKey: "reference",
+    title: "Référence",
+    enableSorting: true,
+    filterFn: multiColumnFilter,
+  }),
+
+  column.createPriceWithBadgeColumn({
+    id: "prix",
+    accessorKey: "prix",
+    title: "Prix",
+    enableSorting: true,
+    devise: "€",
+    variantFn: (row) => "green",
+    customPrice: (datas) => Number(getTotalOrderPrice(datas.orderItems)),
+  }),
+
+  column.createTwoDateColumn({
+    id: "dates",
+    accessorKey: "startsAt",
+    secondAccessorKey: "endsAt",
+    title: "Début / Fin",
+    enableSorting: true,
   }),
 
   column.createDateColumn({
