0f5c3cb91d55084aa236c0003d50af223fee040d
L0Bot <botterilouis@gmail.com>
2025-08-28 16:08:55 +0200

    prettier & lint

--- backend/src/migrations/1756377473882-voucher_test.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class VoucherTest1756377473882 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
